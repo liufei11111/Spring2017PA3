7a3a36cedf7c26df5bc20566e0d166c1d8b31eaf
ZhouyuanLi <lizhouyuandream@gmail.com>
2017-05-15 21:51:59 -0700

    change pagerank function

--- src/edu/stanford/cs276/BM25Scorer.java
+++ src/edu/stanford/cs276/BM25Scorer.java
@@ -139,7 +139,8 @@ public class BM25Scorer extends AScorer {
   }
   
   double getPageRankScores(int pageRank) {
-    return Math.log(pageRankLambdaPrime + pageRank);
+    return (double)pageRank / (pageRankLambdaPrime + pageRank);
+    //return Math.log(pageRankLambdaPrime + pageRank);
   }
   
   double getUrlLength(String s) {
